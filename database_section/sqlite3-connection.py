a028b2fc1b4c5777d0e9251841381f68c4fddbdd
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-06-05 10:50:58 +0200

    Connecting and inserting data to SQLite via Python

---
+++ database_section/sqlite3-connection.py
@@ -0,0 +1,51 @@
+'''
+This program allows to connect to an
+SQLite database.
+
+@author Rodriguez Acosta Ernesto Antonio
+'''
+import sqlite3
+
+def create_table():
+    #1) Create the database connection
+    #   - If we don't have the database file yet, it will be created
+    #     by this line of code and the connection will be established.
+    db_connection = sqlite3.connect("lite.db")
+
+    #2) Create the cursor object
+    cursor = db_connection.cursor()
+
+    #3) Writing the SQL query
+    cursor.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
+
+    #4) Commit the changes
+    db_connection.commit()
+
+    #5) Close the database connection
+    db_connection.close()
+
+
+def insert_data(item, quantity, price):
+    db_connection = sqlite3.connect("lite.db")
+
+    cursor = db_connection.cursor()
+    cursor.execute("INSERT INTO store VALUES(?, ?, ?)", (item, quantity, price))
+
+    db_connection.commit()
+    db_connection.close()
+
+def view():
+    db_connection = sqlite3.connect("lite.db")
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM store")
+    store_db_data = cursor.fetchall()
+
+    db_connection.close()
+
+    return store_db_data    # The variable is returned as a list
+
+
+create_table()
+insert_data("Water Glass", 10, 5)
+insert_data("Steel Glass", 11, 5.50)
+print(view())
