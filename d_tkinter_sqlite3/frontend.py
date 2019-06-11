099f9f1ab8065456523bcb86379918314a5ea05f
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-06-11 18:16:14 +0200

    Converting frontend.py to OOP style.

--- d_tkinter_sqlite3/frontend.py
+++ d_tkinter_sqlite3/frontend.py
@@ -21,145 +21,148 @@ from backend import Database
 
 database = Database('books.db')
 
+class BookStoreFrontend:
 
-def get_selected_row(event):
-    try:
-        global selected_tuple
-        index = books_listbox.curselection()[0]
-        selected_tuple = books_listbox.get(index)
+    def __init__(self):
+        window = Tk()
+        window.wm_title("Book Store")
+        window.geometry("")
 
-        title_entry.delete(0, END)
-        author_entry.delete(0, END)
-        year_entry.delete(0, END)
-        isbn_entry.delete(0, END)
+        # Creating the labels
+        title_label = Label(window, text="Title")
+        title_label.grid(row=0, column=0)
 
-        title_entry.insert(END, selected_tuple[1])
-        author_entry.insert(END, selected_tuple[2])
-        year_entry.insert(END, selected_tuple[3])
-        isbn_entry.insert(END, selected_tuple[4])
-    except IndexError:
-        pass
-'''
-Wrapper function for the database's view function.
-'''
-def view_command():
-    books_listbox.delete(0, END)        # Cleaning the list box content
-    for row in database.view():
-        books_listbox.insert(END, row)
+        year_label = Label(window, text="Year")
+        year_label.grid(row=1, column=0)
 
-'''
-Wrapper function for the database's search function.
-'''
-def search_command():
-    books_listbox.delete(0, END)
+        author_label = Label(window, text="Author")
+        author_label.grid(row=0, column=2)
 
-    for row in database.search(title_data.get(), author_data.get(), year_data.get(), isbn_data.get()):
-        books_listbox.insert(END, row)
+        isbn_label = Label(window, text="ISBN")
+        isbn_label.grid(row=1, column=2)
 
-    if (books_listbox.size() == 0):
-        messagebox.showerror("Error", "There isn't any book that satisfy the search criteria.")
+        # Creating the entries
+        self.title_data = StringVar();
+        title_entry = Entry(window, textvariable=self.title_data)
+        title_entry.grid(row=0, column=1)
 
-'''
-Wrapper function for the database's insert function.
-'''
-def insert_command():
-    books_listbox.delete(0, END)
+        self.year_data = IntVar()
+        year_entry = Entry(window, textvariable=self.year_data)
+        year_entry.grid(row=1, column=1)
 
-    books_list = database.search(title_data.get(), author_data.get(), year_data.get(), isbn_data.get())
+        self.author_data = StringVar()
+        author_entry = Entry(window, textvariable=self.author_data)
+        author_entry.grid(row=0, column=3)
 
-    if(len(books_list) == 0):
-        database.insert(title_data.get(), author_data.get(), year_data.get(), isbn_data.get())
-    else:
-        messagebox.showerror("Error", "This book already exists")
+        self.isbn_data = IntVar()
+        isbn_entry = Entry(window, textvariable=self.isbn_data)
+        isbn_entry.grid(row=1, column=3)
 
-    view_command()
+        # Creating the listbox
+        self.books_listbox = Listbox(window, height=6, width=35)
+        self.books_listbox.grid(row=2, column=0, rowspan=6, columnspan=2)
 
-'''
-Wrapper function for the database's delete function.
-'''
-def delete_command():
-    if not books_listbox.curselection():
-        messagebox.showerror("Error", "Select a valid entry to delete.")
-    else:
-        database.delete(selected_tuple[0])
-        view_command()
-        messagebox.showinfo("Info", "Entry deleted.")
+        # Creating the scrollbar
+        scroll_bar = Scrollbar(window)
+        scroll_bar.grid(row=2, column=2, rowspan=6)
 
-'''
-Wrapper function for the database's update function.
-'''
-def update_command():
-    database.update(selected_tuple[0], title_data.get(), author_data.get(), year_data.get(), isbn_data.get())
-    view_command()
-    messagebox.showinfo("Info", "Entry updated.")
+        # Linking listbox with the scroll bar
+        self.books_listbox.configure(yscrollcommand=scroll_bar.set)
+        scroll_bar.configure(command=self.books_listbox.yview)
 
+        #Binding the ListboxSelect event to a function
+        self.books_listbox.bind("<<ListboxSelect>>", self.get_selected_row)
 
-window = Tk()
-window.wm_title("Book Store")
-window.geometry("")
+        # Creating the buttons
+        view_btn = Button(window, text="View all", width=12, command=self.view_command)
+        view_btn.grid(row=2, column=3)
 
-# Creating the labels
-title_label = Label(window, text="Title")
-title_label.grid(row=0, column=0)
+        search_btn = Button(window, text="Search entry", width=12, command=self.search_command)
+        search_btn.grid(row=3, column=3)
 
-year_label = Label(window, text="Year")
-year_label.grid(row=1, column=0)
+        add_btn = Button(window, text="Add entry", width=12, command=self.insert_command)
+        add_btn.grid(row=4, column=3)
 
-author_label = Label(window, text="Author")
-author_label.grid(row=0, column=2)
+        update_btn = Button(window, text="Update", width=12, command=self.update_command)
+        update_btn.grid(row=5, column=3)
 
-isbn_label = Label(window, text="ISBN")
-isbn_label.grid(row=1, column=2)
+        delete_btn = Button(window, text="Delete", width=12, command=self.delete_command)
+        delete_btn.grid(row=6, column=3)
 
-# Creating the entries
-title_data = StringVar();
-title_entry = Entry(window, textvariable=title_data)
-title_entry.grid(row=0, column=1)
+        close_btn = Button(window, text="Close", width=12, command=window.destroy)
+        close_btn.grid(row=7, column=3)
 
-year_data = IntVar()
-year_entry = Entry(window, textvariable=year_data)
-year_entry.grid(row=1, column=1)
+        window.mainloop()
 
-author_data = StringVar()
-author_entry = Entry(window, textvariable=author_data)
-author_entry.grid(row=0, column=3)
+    def get_selected_row(self, event):
+        try:
+            global selected_tuple
+            index = self.books_listbox.curselection()[0]
+            selected_tuple = self.books_listbox.get(index)
 
-isbn_data = IntVar()
-isbn_entry = Entry(window, textvariable=isbn_data)
-isbn_entry.grid(row=1, column=3)
+            self.title_entry.delete(0, END)
+            self.author_entry.delete(0, END)
+            self.year_entry.delete(0, END)
+            self.isbn_entry.delete(0, END)
 
-# Creating the listbox
-books_listbox = Listbox(window, height=6, width=35)
-books_listbox.grid(row=2, column=0, rowspan=6, columnspan=2)
+            self.title_entry.insert(END, selected_tuple[1])
+            self.author_entry.insert(END, selected_tuple[2])
+            self.year_entry.insert(END, selected_tuple[3])
+            self.isbn_entry.insert(END, selected_tuple[4])
+        except IndexError:
+            pass
+    '''
+    Wrapper function for the database's view function.
+    '''
+    def view_command(self):
+        self.books_listbox.delete(0, END)        # Cleaning the list box content
+        for row in database.view():
+            self.books_listbox.insert(END, row)
 
-# Creating the scrollbar
-scroll_bar = Scrollbar(window)
-scroll_bar.grid(row=2, column=2, rowspan=6)
+    '''
+    Wrapper function for the database's search function.
+    '''
+    def search_command(self):
+        self.books_listbox.delete(0, END)
 
-# Linking listbox with the scroll bar
-books_listbox.configure(yscrollcommand=scroll_bar.set)
-scroll_bar.configure(command=books_listbox.yview)
+        for row in database.search(self.title_data.get(), self.author_data.get(), self.year_data.get(), self.isbn_data.get()):
+            self.books_listbox.insert(END, row)
 
-#Binding the ListboxSelect event to a function
-books_listbox.bind("<<ListboxSelect>>", get_selected_row)
+        if (self.books_listbox.size() == 0):
+            messagebox.showerror("Error", "There isn't any book that satisfy the search criteria.")
 
-# Creating the buttons
-view_btn = Button(window, text="View all", width=12, command=view_command)
-view_btn.grid(row=2, column=3)
+    '''
+    Wrapper function for the database's insert function.
+    '''
+    def insert_command(self):
+        self.books_listbox.delete(0, END)
 
-search_btn = Button(window, text="Search entry", width=12, command=search_command)
-search_btn.grid(row=3, column=3)
+        books_list = database.search(self.title_data.get(), self.author_data.get(), self.year_data.get(), self.isbn_data.get())
 
-add_btn = Button(window, text="Add entry", width=12, command=insert_command)
-add_btn.grid(row=4, column=3)
+        if(len(books_list) == 0):
+            database.insert(self.title_data.get(), self.author_data.get(), self.year_data.get(), self.isbn_data.get())
+        else:
+            messagebox.showerror("Error", "This book already exists")
 
-update_btn = Button(window, text="Update", width=12, command=update_command)
-update_btn.grid(row=5, column=3)
+        self.view_command()
 
-delete_btn = Button(window, text="Delete", width=12, command=delete_command)
-delete_btn.grid(row=6, column=3)
+    '''
+    Wrapper function for the database's delete function.
+    '''
+    def delete_command(self):
+        if not self.books_listbox.curselection():
+            messagebox.showerror("Error", "Select a valid entry to delete.")
+        else:
+            database.delete(selected_tuple[0])
+            self.view_command()
+            messagebox.showinfo("Info", "Entry deleted.")
 
-close_btn = Button(window, text="Close", width=12, command=window.destroy)
-close_btn.grid(row=7, column=3)
+    '''
+    Wrapper function for the database's update function.
+    '''
+    def update_command(self):
+        database.update(selected_tuple[0], self.title_data.get(), self.author_data.get(), self.year_data.get(), self.isbn_data.get())
+        self.view_command()
+        messagebox.showinfo("Info", "Entry updated.")
 
-window.mainloop()
+bookStore = BookStoreFrontend()
