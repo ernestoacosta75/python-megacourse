125463cc3a19eb01e8bf8258be00193dcfa516ae
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-06-11 17:21:56 +0200

    Binding frontend to backend. Added try-catch block to get_selected_row function.

--- d_tkinter_sqlite3/backend.py
+++ d_tkinter_sqlite3/backend.py
@@ -8,33 +8,38 @@ class Database:
     '''
     def __init__(self, db_name):
         self.db_name = db_name
-        connection_database = sqlite3.connect(self.db_name)
-        cursor = connection_database.cursor()
+        database_connection, cursor = self.open_connection()
         cursor.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
-        connection_database.commit()
-        connection_database.close()
+        self.close_connection(database_connection)
+
+    def open_connection(self):
+        database_connection = sqlite3.connect(self.db_name)
+        cursor = database_connection.cursor()
+
+        return database_connection, cursor
+
+    def close_connection(self, database_connection):
+        database_connection.commit()
+        database_connection.close()
 
     '''
     Inserting a book in the database.
     '''
     def insert(self, title, author, year, isbn):
-        connection_database = sqlite3.connect(self.db_name)
-        cursor = connection_database.cursor()
+        database_connection, cursor = self.open_connection()
         cursor.execute("INSERT INTO book VALUES(NULL, ?, ?, ?, ?)", (title, author, year, isbn))
-        connection_database.commit()
-        connection_database.close()
+        self.close_connection(database_connection)
 
     '''
     Select all books from the database.
     '''
     def view(self):
-        connection_database = sqlite3.connect(self.db_name)
-        cursor = connection_database.cursor()
+        database_connection, cursor = self.open_connection()
         cursor.execute("SELECT * FROM book")
 
         books_list = cursor.fetchall()
 
-        connection_database.close()
+        self.close_connection(database_connection)
 
         return books_list
 
@@ -42,13 +47,12 @@ class Database:
     Search for a book in the database.
     '''
     def search(self, title="", author="", year="", isbn=""):
-        connection_database = sqlite3.connect(self.db_name)
-        cursor = connection_database.cursor()
+        database_connection, cursor = self.open_connection()
         cursor.execute("SELECT * FROM book WHERE title = ? OR author = ? OR year = ? OR isbn = ?", (title, author, year, isbn))
 
         books_list = cursor.fetchall()
 
-        connection_database.close()
+        self.close_connection(database_connection)
 
         return books_list
 
@@ -56,18 +60,16 @@ class Database:
     Delete a book in the database.
     '''
     def delete(self, book_id):
-        connection_database = sqlite3.connect(self.db_name)
-        cursor = connection_database.cursor()
+        database_connection, cursor = self.open_connection()
         cursor.execute("DELETE FROM book WHERE id = ?", (book_id,))
-        connection_database.commit()
-        connection_database.close()
+
+        self.close_connection(database_connection)
 
     '''
     Update a book in the database.
     '''
     def update(self, id, title, author, year, isbn):
-        connection_database = sqlite3.connect(self.db_name)
-        cursor = connection_database.cursor()
+        database_connection, cursor = self.open_connection()
         cursor.execute("UPDATE book SET title = ?, author = ?, year = ?, isbn = ? WHERE id = ?", (title, author, year, isbn, id))
-        connection_database.commit()
-        connection_database.close()
+
+        self.close_connection(database_connection)
